00ba67580d5b7b5a4e25348cddb0740f572963b0
Bridget Sarah <msbskelly@gmail.com>
2019-03-07 17:41:16 +0000

    Building Backend

---
+++ Admin/admin.py
@@ -0,0 +1,35 @@
+
+
+admin_dashboard = ('admin_dash')
+client_dashboard = ('client_dash')
+settings = ('settings')
+
+# Administration Dashboard Menu
+
+def admin_dash ():
+
+    analytics = ('analytics')
+    clients = ('clients')
+    projects = ('projects')
+    settings ('settings')
+    return admin_dash()
+
+print (admin_dash())
+
+# Analytics Menu
+def admin_analytics():
+    traffic = ('traffic')
+    return('admin_analytics')
+print(admin_analytics())
+
+# Client Mnenu
+def admin_clients():
+    account_id = ('INT')
+    account_name = ('')
+    website_status = ('INT')
+    domain_name = ('')
+    return (admin_clients())
+print (admin_clients())
+
+
+
